4d2e976551c95d01337c527e73e495665319bec6
Ryan Huynh <ryanhuynh@mu-582884.dhcp.missouri.edu>
2022-02-02 09:04:04 -0600

    using requests.get

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-s = requests.Session()
-
 url = "https://bsd.sos.mo.gov/BusinessEntity/BESearch.aspx"
-#page = s.get(url)
-requests.post(url)
-#print(page.text)
+page = requests.get(url)
+print(page.text)
