344ee73712ce514bdda80505696ebd2e1ed385dd
Ryan Huynh <ryanhuynh@mu-582884.dhcp.missouri.edu>
2022-02-02 09:04:42 -0600

    using urlopen

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 url = "https://bsd.sos.mo.gov/BusinessEntity/BESearch.aspx"
-page = requests.get(url)
-print(page.text)
+
+page = urlopen(url)
+print(page)
+
+html_bytes = page.read()
+html = html_bytes.decode("UTF-8")
+print(html)
